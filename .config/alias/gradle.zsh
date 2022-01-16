aa416a369eb09a4d1270c42429d367a8cdd9df67
Tung Beier <beiertu@mediamarktsaturn.com>
2022-01-16 15:54:05 +0100

    Use installed gradle in alias and pre-commit

--- .config/alias/gradle.zsh
+++ .config/alias/gradle.zsh
@@ -1,6 +1,6 @@
 alias gradle='gradle --console=rich'
-alias gw="./gradlew --console=rich"
-alias gwc="./gradlew --console=rich clean"
-alias gwb="./gradlew --console=rich build"
-alias gwcb="./gradlew --console=rich clean build"
+alias gw="gradle --console=rich"
+alias gwc="gradle --console=rich clean"
+alias gwb="gradle --console=rich build"
+alias gwcb="gradle --console=rich clean build"
 
