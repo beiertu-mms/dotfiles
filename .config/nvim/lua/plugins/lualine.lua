d188b7c291a7bcae3fa275951c843379913ea4dd
Tung Beier <beiertu@mediamarktsaturn.com>
2022-02-05 22:18:11 +0100

    Update lualine configuration

--- .config/nvim/lua/plugins/lualine.lua
+++ .config/nvim/lua/plugins/lualine.lua
@@ -4,31 +4,51 @@ if not status_ok then
   return
 end
 
+local filetype = {
+  'filetype',
+  colored = false,   -- Displays filetype icon in color if set to true
+  icon_only = true -- Display only an icon for filetype
+}
+
+local filename = {
+  'filename',
+  file_status = true,   -- Displays file status (readonly status, modified status)
+  path = 1,             -- 0: Just the filename, 1: Relative path, 2: Absolute path
+  shorting_target = 50, -- Shortens path to leave 50 spaces in the window
+  symbols = {
+    modified = ' ',
+    readonly = ' ',
+    unnamed = 'No Name',
+  }
+}
+
 lualine.setup {
   options = {
     icons_enabled = true,
-    theme = 'wombat',
-    component_separators = { left = '', right = ''},
-    section_separators = { left = '', right = ''},
-    disabled_filetypes = {},
+    theme = "wombat",
+    component_separators = { left = '', right = '' },
+    section_separators = { left = '', right = '' },
+    disabled_filetypes = { "alpha", "dashboard", "NvimTree", "Outline" },
     always_divide_middle = true,
   },
   sections = {
-    lualine_a = {'mode'},
-    lualine_b = {'branch', 'diff', 'diagnostics'},
-    lualine_c = {'filename'},
-    lualine_x = {'encoding', 'fileformat', 'filetype'},
-    lualine_y = {'progress'},
-    lualine_z = {'location'}
+    lualine_a = { 'mode' },
+    lualine_b = { filetype, filename },
+    lualine_c = { 'diagnostics' },
+
+    lualine_x = { 'diff', 'encoding', 'fileformat' },
+    lualine_y = { 'branch', 'progress' },
+    lualine_z = { 'location' },
   },
   inactive_sections = {
     lualine_a = {},
     lualine_b = {},
-    lualine_c = {'filename'},
-    lualine_x = {'location'},
+    lualine_c = { "filename" },
+    lualine_x = { "location" },
     lualine_y = {},
-    lualine_z = {}
+    lualine_z = {},
   },
   tabline = {},
-  extensions = {}
+  extensions = {},
 }
+
