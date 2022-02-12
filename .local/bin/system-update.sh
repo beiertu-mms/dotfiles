115f96bb3d5f47f64b8a6d14fbe8b08ff46d1a69
Tung Beier <beiertu@mediamarktsaturn.com>
2022-02-12 23:34:40 +0100

    Update cheat binary with go install

--- .local/bin/system-update.sh
+++ .local/bin/system-update.sh
@@ -12,8 +12,8 @@
 #       CREATED: 28 May 2021 20:29 CEST
 #===============================================================================
 
-set -o errexit  # Exit when a command fails
-                # Use || true if a command is allowed to fail
+set -o errexit # Exit when a command fails
+# Use || true if a command is allowed to fail
 set -o nounset  # Treat unset variables as an error
 set -o pipefail # Exit when a command in a pipeline fails
 
@@ -21,39 +21,35 @@ GREEN='\033[0;32m'
 NC='\033[0m' # No Color
 
 function print() {
-    echo -e "\n${GREEN}${1}${NC}\n"
+	echo -e "\n${GREEN}${1}${NC}\n"
 }
 
-if command -v gcloud &> /dev/null
-then
-    print "update gcloud"
-    gcloud components update
+if command -v gcloud &>/dev/null; then
+	print "update gcloud"
+	gcloud components update
 fi
 
-if command -v ccloud &> /dev/null
-then
-    print "update ccloud"
-    ccloud update
+if command -v ccloud &>/dev/null; then
+	print "update ccloud"
+	ccloud update
 fi
 
-if command -v confluent &> /dev/null
-then
-    print "update confluent"
-    confluent update
+if command -v confluent &>/dev/null; then
+	print "update confluent"
+	confluent update
 fi
 
-if command -v cheatsheets &> /dev/null
-then
-    print "update cheatsheets"
-    cheatsheets pull
+if command -v cheatsheets &>/dev/null; then
+	go install github.com/cheat/cheat/cmd/cheat@latest
+	print "update cheatsheets"
+	cheatsheets pull
 fi
 
 curl -LJO https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/plugins/ssh-agent/ssh-agent.plugin.zsh \
-  --output-dir "$HOME/.local/share/zsh-ssh-agent/"
+	--output-dir "$HOME/.local/share/zsh-ssh-agent/"
 
 print "update arch"
 yay -Syyu
 
 print "done"
 print "TODO update go installed packages"
-
