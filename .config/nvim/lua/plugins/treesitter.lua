99be3c3500b4e832f9ba172c0290aa9c4c414a5a
Tung Beier <beiertu@mediamarktsaturn.com>
2022-04-11 21:27:29 +0200

    Replace treesitter deprecated maintained option
    See https://github.com/nvim-treesitter/nvim-treesitter/commit/acd01551a3185118f58f0708b7fffdb2465e39f1

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -7,7 +7,39 @@ if not status_ok then
 end
 
 treesitter_conf.setup({
-	ensure_installed = "maintained", -- One of "all", "maintained" (parsers with maintainers), or a list of languages
+	-- https://github.com/nvim-treesitter/nvim-treesitter#supported-languages
+	ensure_installed = {
+		"c",
+		"bash",
+		"comment",
+		"cpp",
+		"css",
+		"dockerfile",
+		"go",
+		"gomod",
+		"gowork",
+		"hcl",
+		"hjson",
+		"hocon",
+		"html",
+		"http",
+		"java",
+		"javascript",
+		"json",
+		"json5",
+		"kotlin",
+		"lua",
+		"make",
+		"python",
+		"rasi",
+		"regex",
+		"scss",
+		"toml",
+		"tsx",
+		"typescript",
+		"vim",
+		"yaml",
+	},
 	sync_install = false, -- Install languages synchronously (only applied to `ensure_installed`)
 	ignore_install = { "" }, -- List of parsers to ignore installing
 
