68f03f66f81c076ce1a403718ad7a080eb873d47
Tung Beier <beiertu@mediamarktsaturn.com>
2023-01-09 19:49:56 +0100

    feat(nvim): Add LSP info to lualine

--- .config/nvim/after/plugin/lualine.lua
+++ .config/nvim/after/plugin/lualine.lua
@@ -25,6 +25,28 @@ local filename = {
   },
 }
 
+local function get_active_lsp()
+  local msg = 'No Active LSP'
+  local buf_ft = vim.api.nvim_buf_get_option(0, 'filetype')
+  local clients = vim.lsp.get_active_clients()
+  if next(clients) == nil then
+    return msg
+  end
+  for _, client in ipairs(clients) do
+    local filetypes = client.config.filetypes
+    if filetypes and vim.fn.index(filetypes, buf_ft) ~= -1 then
+      return client.name
+    end
+  end
+  return msg
+end
+
+local lsp = {
+  get_active_lsp,
+  icon = ' LSP:',
+  color = { fg = 'teal', gui = 'bold' },
+}
+
 local custom_onedark = require('lualine.themes.onedark')
 custom_onedark.normal.a.bg = '#61AFEF'
 custom_onedark.insert.a.bg = '#98C379'
@@ -39,11 +61,11 @@ lualine.setup({
     always_divide_middle = true,
   },
   sections = {
-    lualine_a = { 'mode' },
+    lualine_a = { { 'mode', fmt = function(str) return str:sub(1,1) end } },
     lualine_b = { filename, 'diagnostics' },
     lualine_c = { 'diff' },
 
-    lualine_x = { 'encoding', 'fileformat', filetype },
+    lualine_x = { lsp, 'encoding', 'fileformat', filetype },
     lualine_y = { 'branch', 'progress' },
     lualine_z = { 'location' },
   },
@@ -59,3 +81,4 @@ lualine.setup({
   tabline = {},
   extensions = {},
 })
+
