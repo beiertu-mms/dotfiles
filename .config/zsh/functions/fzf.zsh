eb5c88badd2e3a0a6a4cdafc4b0df2b90e46fcb5
Tung Beier <beiertu@mediamarktsaturn.com>
2023-05-12 17:44:04 +0200

    refactor(zsh): use an util funtion for searching

--- .config/zsh/functions/fzf.zsh
+++ .config/zsh/functions/fzf.zsh
@@ -1,19 +1,25 @@
 #-------------------------------------------------------------------------------
+# Search with fd and return the selected value from fzf.
+#-------------------------------------------------------------------------------
+_search_with_fzf() {
+  local selected
+  selected=$(fd -E .git -E target -E build -E out -E .cache -H "$@" | fzf)
+  echo "$selected"
+}
+
+#-------------------------------------------------------------------------------
 # Search for a folder and cd into it.
 #-------------------------------------------------------------------------------
 function sd() {
-  local selected
-  selected=$(fd -t d -E .git -E target -E build -E out -E .cache -H "$@" | fzf)
+  local -r selected=$(_search_with_fzf -t d "$@")
   [[ -n "$selected" ]] && cd "$selected" || return 0
 }
 
 #-------------------------------------------------------------------------------
-# Search for a file and open it in nvim.
+# Search for a file and open it with $EDITOR.
 #-------------------------------------------------------------------------------
 function sf() {
-  local selected
-  selected=$(fd -t f -E .git -E target -E build -E out -E .cache -H "$@" | fzf)
-    # | fzf --preview-window right:50% --preview 'bat --color=always --line-range :500 {}')
+  local -r selected=$(_search_with_fzf -t f "$@")
   [[ -n "$selected" ]] && $EDITOR "$selected" || return 0
 }
 
