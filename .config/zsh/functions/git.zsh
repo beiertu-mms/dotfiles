041b79e35d9d517d2c4c45a296a65dc6178d35c1
Tung Beier <beiertu@mediamarktsaturn.com>
2023-05-18 18:49:06 +0200

    feat(zsh): add git function gcl

--- .config/zsh/functions/git.zsh
+++ .config/zsh/functions/git.zsh
@@ -64,3 +64,28 @@ function gbase() {
     | sed 's/.*\[\(.*\)\].*/\1/' \
     | sed 's/[\^~].*//'
 }
+
+# Usage:       gcl $1 $2
+# Description: Clone a git repository based on its host, owner and name.
+# Params:
+#   - $1: (required) A git repository url.
+#   - $2: The base folder to which the repo will be cloned. Default: $HOME/data.
+# See: https://serverfault.com/questions/417241/extract-repository-name-from-github-url-in-bash
+function gcl() {
+  local url=${1:?require a git repo url as the first parameter}
+  local base_folder=${2:-"$HOME"/data}
+
+  local git_url_regex="^(https|git)(:\/\/|@)([^\/:]+)[\/:]([^\/:]+)\/(.+)(.git)*$"
+  if [[ ! $url =~ $git_url_regex ]]; then
+    echo -e "${RED:-}given url $url does not match: $git_url_regex ${NC:-}"
+    return 1
+  fi
+
+  local hostname=${BASH_REMATCH[3]}
+  local user=${BASH_REMATCH[4]}
+  local repo=${BASH_REMATCH[5]}
+
+  mkdir --parents "$base_folder/$hostname/$user/${repo%.git}" \
+      && git clone "$url" "$folder"
+}
+
