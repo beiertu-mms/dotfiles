18cb4cceef18a3aaa123b562aef050b4cac8a7b6
Tung Beier <beiertu@mediamarktsaturn.com>
2023-05-24 20:59:12 +0200

    feat(zsh): simplify maven aliases

--- .config/zsh/aliases/maven.zsh
+++ .config/zsh/aliases/maven.zsh
@@ -4,24 +4,10 @@
 #  | | | | | | | (_| |  \ V /  |  __/ | | | |   | (_| | | | | | | (_| | \__ \
 #  |_| |_| |_|  \__,_|   \_/    \___| |_| |_|    \__,_| |_| |_|  \__,_| |___/
 #-------------------------------------------------------------------------------
-alias m='mvn'
+alias m='mvn -ntp'
 
 alias mc='mvn clean'
-alias mcp='mvn compile'
 
-alias mp='mvn package'
-alias mpo='mvn -o package'
-alias mpf='mvn -T 1C package'
-alias mpof='mvn -o -T 1C package'
-alias mpont='mvn -o -Dmaven.test.skip=true package'
-alias mpnt='mvn -Dmaven.test.skip=true package'
-
-alias mi='mvn install'
-alias mio='mvn -o install'
-alias mif='mvn -T 1C install'
-alias miof='mvn -o -T 1C install'
-alias miont='mvn -o -Dmaven.test.skip=true install'
-alias mint='mvn -Dmaven.test.skip=true install'
-
-alias mifnt='mvn -o -T 1C -Dmaven.test.skip=true install'
+alias mci='mvn clean install'
+alias mcis='mvn clean install -Dmaven.test.skip=true'
 
