1061798c2c6f3fc1ea58ef77ac5602049ceb2eba
Tung Beier <beiertu@mediamarktsaturn.com>
2023-08-15 20:25:54 +0200

    feat(nvim): configure lsp keymaps
    Take key mappings for lsp from
    - https://github.com/nvim-lua/kickstart.nvim/blob/master/init.lua

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -23,8 +23,27 @@ return {
     -- Language Servers configuration
     local lsp = require('lsp-zero').preset({})
 
-    lsp.on_attach(function(client, bufnr)
-      lsp.default_keymaps({ buffer = bufnr })
+    lsp.on_attach(function(_, bufnr)
+      local nmap = function(keys, func, desc)
+        if desc then
+          desc = 'LSP: ' .. desc
+        end
+
+        vim.keymap.set('n', keys, func, { buffer = bufnr, desc = desc })
+      end
+      nmap('<leader>rn', vim.lsp.buf.rename, '[R]e[n]ame')
+      nmap('<leader>ca', vim.lsp.buf.code_action, '[C]ode [A]ction')
+      nmap('<leader>D', vim.lsp.buf.type_definition, 'Type [D]efinition')
+      nmap('<leader>ds', require('telescope.builtin').lsp_document_symbols, '[D]ocument [S]ymbols')
+      nmap('<leader>ws', require('telescope.builtin').lsp_dynamic_workspace_symbols, '[W]orkspace [S]ymbols')
+
+      nmap('gd', vim.lsp.buf.definition, '[G]oto [D]efinition')
+      nmap('gr', require('telescope.builtin').lsp_references, '[G]oto [R]eferences')
+      nmap('gD', vim.lsp.buf.declaration, '[G]oto [D]eclaration')
+      nmap('gI', vim.lsp.buf.implementation, '[G]oto [I]mplementation')
+
+      nmap('K', vim.lsp.buf.hover, 'Hover Documentation') -- See `:help K` for why this keymap
+      nmap('<C-k>', vim.lsp.buf.signature_help, 'Signature Documentation')
     end)
 
     -- https://github.com/williamboman/mason-lspconfig.nvim#available-lsp-servers
