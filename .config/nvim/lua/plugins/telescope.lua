f8bffbbdbc0a6b2f15965552fbb30bf33d8ac261
Tung Beier <beiertu@mediamarktsaturn.com>
2023-08-15 21:47:38 +0200

    refactor(nvim): adjust telescope layout and fzf extension
    See:
    - https://github.com/nvim-telescope/telescope.nvim#customization
    - https://github.com/nvim-telescope/telescope-fzf-native.nvim

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -1,5 +1,6 @@
 return {
   'nvim-telescope/telescope.nvim', -- https://github.com/nvim-telescope/telescope.nvim
+  branch = '0.1.x',
   cmd = 'Telescope',
   keys = {
     { '<C-s>', '<cmd>Telescope buffers<cr>', desc = 'Open files in buffers' },
@@ -19,10 +20,7 @@ return {
     {
       '<leader>/',
       function()
-        require('telescope.builtin').current_buffer_fuzzy_find(require('telescope.themes').get_dropdown({
-          winblend = 10,
-          previewer = false,
-        }))
+        require('telescope.builtin').current_buffer_fuzzy_find()
       end,
       { desc = '[/] Fuzzily search in current buffer' },
     },
@@ -31,41 +29,45 @@ return {
     { '<leader>sh', '<cmd>Telescope help_tags<cr>', desc = '[S]earch [H]elp' },
     { '<leader>sw', '<cmd>Telescope grep_string<cr>', desc = '[S]earch current [W]ord' },
   },
-  dependencies = { 'nvim-lua/plenary.nvim', lazy = true },
-  opts = {
-    defaults = {
-      prompt_prefix = '  ',
-      selection_caret = '▶ ',
-      path_display = { 'smart' },
+  dependencies = {
+    { 'nvim-lua/plenary.nvim', lazy = true },
+    {
+      'nvim-telescope/telescope-fzf-native.nvim',
+      build = 'cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release && cmake --install build --prefix build',
+      lazy = true,
     },
-    pickers = {
-      find_files = {
-        theme = 'dropdown',
-        previewer = false,
-        layout_config = {
-          height = 0.50,
-          width = 0.80,
-        },
-        find_command = { 'rg', '--files', '--hidden', '--glob', '!**/.git/*' },
-      },
-      git_files = {
-        theme = 'dropdown',
-        previewer = false,
-        layout_config = {
-          height = 0.50,
-          width = 0.80,
-        },
-        find_command = { 'rg', '--files', '--hidden', '--glob', '!**/.git/*' },
-      },
-      buffers = {
-        theme = 'dropdown',
-        previewer = false,
-        layout_config = {
-          height = 0.50,
-          width = 0.80,
-        },
-      },
-    },
-    extensions = {},
   },
+  config = function()
+    local telescope = require('telescope')
+
+    telescope.setup({
+      defaults = {
+        prompt_prefix = '  ',
+        selection_caret = '▶ ',
+        path_display = { 'smart' },
+        preview = {
+          hide_on_startup = true,
+        },
+        layout_strategy = 'horizontal',
+        layout_config = {
+          horizontal = {
+            height = 0.60,
+            width = 0.80,
+            preview_cutoff = 80,
+            prompt_position = 'bottom',
+          },
+        },
+      },
+      extensions = {
+        fzf = {
+          fuzzy = true,
+          override_generic_sorter = true,
+          override_file_sorter = true,
+          case_mode = 'smart_case',
+        },
+      },
+    })
+
+    telescope.load_extension('fzf')
+  end,
 }
