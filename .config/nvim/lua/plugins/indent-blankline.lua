38c7b9d981a4a93f058aad972a6fca3a5ff8f796
Tung Beier <beiertu@mediamarktsaturn.com>
2023-10-05 15:34:06 +0200

    feat(nvim): adapt indent-blankline to v3
    See also
    - https://github.com/lukas-reineke/indent-blankline.nvim/wiki/Migrate-to-version-3

--- .config/nvim/lua/plugins/indent-blankline.lua
+++ .config/nvim/lua/plugins/indent-blankline.lua
@@ -1,11 +1,6 @@
 return {
   'lukas-reineke/indent-blankline.nvim', -- https://github.com/lukas-reineke/indent-blankline.nvim
+  main = 'ibl',
   event = { 'BufReadPost', 'BufNewFile' },
-  opts = {
-    show_current_context = true,
-    show_current_context_start = true,
-    show_end_of_line = true,
-    show_trailing_blankline_indent = false,
-    filetype_exclude = { 'help', 'alpha', 'dashboard', 'neo-tree', 'Trouble', 'lazy', 'mason' },
-  },
+  opts = {},
 }
