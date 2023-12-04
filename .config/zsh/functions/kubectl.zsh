89c46a1c656058fdc42bf70daa4b65ede354609c
Tung Beier <beiertu@mediamarktsaturn.com>
2023-12-04 21:37:33 +0100

    feat(zsh): add function to decrypt sops secrets

--- .config/zsh/functions/kubectl.zsh
+++ .config/zsh/functions/kubectl.zsh
@@ -1,22 +1,47 @@
 #-------------------------------------------------------------------------------
-# Select a service to decode all its secrets.
+# Select a service to decode all its secrets using kubeseal.
 #-------------------------------------------------------------------------------
 function un-kubeseal() {
 
-  TEMPLATE=$(cat <<-EOM
+    TEMPLATE=$(
+        cat <<-EOM
 {{ range \$key, \$value := .data }}
   {{- \$key }}: {{ \$value | base64decode }}
 {{ end }}
 EOM
-)
+    )
 
-  kubectl get secrets "$@" -o template="$TEMPLATE"
+    kubectl get secrets "$@" -o template="$TEMPLATE"
 }
 
 #-------------------------------------------------------------------------------
 # Select a service to decode all its secrets.
 #-------------------------------------------------------------------------------
 function un-kubeseal-fzf() {
-  un-kubeseal $(kubectl get service "$@" | tail -n +2 | cut -d' ' -f1 | fzf)
+    un-kubeseal $(kubectl get service "$@" | tail -n +2 | cut -d' ' -f1 | fzf)
 }
 
+#-------------------------------------------------------------------------------
+# Select a service to decode all its secrets using sops .
+#-------------------------------------------------------------------------------
+function un-sops() {
+
+    TEMPLATE=$(
+        cat <<-EOM
+{{ range \$key, \$value := .data }}
+  {{- \$key }}: {{ \$value | base64decode }}
+{{ end }}
+EOM
+    )
+
+    kubectl get secrets "$@" -o template="$TEMPLATE"
+}
+
+#-------------------------------------------------------------------------------
+# Select a secret to decode.
+#-------------------------------------------------------------------------------
+function un-sops-fzf() {
+    un-sops $(kubectl get secrets "$@" -o=custom-columns='NAME:.metadata.name' \
+        --no-headers=true | cut -d' ' -f1 | fzf) \
+        "$@"
+}
