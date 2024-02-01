373d74de07af392fb874001c98b5bcb7947fdbb0
Tung Beier <beiertu@mediamarktsaturn.com>
2024-02-01 14:55:11 +0100

    feat(zsh): add maven verify aliases

--- .config/zsh/aliases/maven.zsh
+++ .config/zsh/aliases/maven.zsh
@@ -7,11 +7,13 @@
 alias m='mvn -ntp'
 alias mc='mvn clean'
 alias mci='mvn clean install'
+alias mcv='mvn clean verify'
 alias mcis='mvn clean install -DskipTests'
 alias mciS='mvn clean install -Dmaven.test.skip=true'
 
 alias md='mvnd -ntp'
 alias mdc='mvnd clean'
 alias mdci='mvnd clean install'
+alias mdcv='mvnd clean verify'
 alias mdcis='mvnd clean install -DskipTests'
 alias mdciS='mvnd clean install -Dmaven.test.skip=true'
