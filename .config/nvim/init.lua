2c6d2244e6a04134a8dacf8f5f438fad96f40da3
Tung Beier <beiertu@mediamarktsaturn.com>
2025-05-02 10:00:25 +0200

    feat(nvim): update lazy.nvim setup
    See also: https://lazy.folke.io/installation

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -13,15 +13,24 @@
 
 -- Setup lazy.nvim - https://github.com/folke/lazy.nvim
 local lazypath = vim.fn.stdpath('data') .. '/lazy/lazy.nvim'
-if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({
+if not vim.uv.fs_stat(lazypath) then
+  local out = vim.fn.system({
     'git',
     'clone',
     '--filter=blob:none',
+    '--branch=stable',
     'https://github.com/folke/lazy.nvim.git',
-    '--branch=stable', -- latest stable release
     lazypath,
   })
+  if vim.v.shell_error ~= 0 then
+    vim.api.nvim_echo({
+      { 'Failed to clone lazy.nvim:\n', 'ErrorMsg' },
+      { out, 'WarningMsg' },
+      { '\nPress any key to exit...' },
+    }, true, {})
+    vim.fn.getchar()
+    os.exit(1)
+  end
 end
 vim.opt.rtp:prepend(lazypath)
 
@@ -29,19 +38,32 @@ vim.opt.rtp:prepend(lazypath)
 vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 
-local lazy_config = {
-  checker = { -- automatically check for plugin updates
-    eabled = true,
+-- Setup lazy.nvim
+require('lazy').setup({
+  spec = {
+    { import = 'plugins' },
+  },
+
+  -- disable LuaRocks (lua package manager because currently none of the installed plugins require it)
+  rocks = {
+    enabled = false,
+  },
+
+  -- automatically check for plugin updates
+  checker = {
+    enabled = true,
+    notify = false,
     frequency = 7200, -- check every 2 hour
   },
+
+  -- automatically check for config file changes and reload the ui
   change_detection = {
     enabled = true,
     notify = false,
   },
-}
+})
 
-require('lazy').setup('plugins', lazy_config)
-
+-- Setup core functionalities
 require('core.options')
 require('core.keymaps')
 require('core.autocmds')
