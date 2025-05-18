976cf06303b37f7757197ce48366425840eac298
Tung Beier <beiertu@mediamarktsaturn.com>
2025-05-18 17:20:50 +0200

    feat(nvim): replace formatter with conform

---
+++ .config/nvim/lua/plugins/conform.lua
@@ -0,0 +1,21 @@
+return {
+  'stevearc/conform.nvim', -- https://github.com/stevearc/conform.nvim
+  event = 'VeryLazy',
+  opts = {
+    formatters_by_ft = {
+      go = { 'gofmt' },
+      json = { 'jq' },
+      kotlin = { 'ktlint' },
+      lua = { 'stylua' },
+      python = { 'black' },
+      sh = { 'shfmt' },
+      terraform = { 'terraform_fmt' },
+      yaml = { 'yamlfmt' },
+    },
+    format_on_save = {
+      -- These options will be passed to conform.format()
+      timeout_ms = 500,
+      lsp_format = 'fallback',
+    },
+  },
+}
