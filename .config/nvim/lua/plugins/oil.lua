7278f06d11cc8aa23178759f72b83d2aaf91e170
Tung Beier <beiertu@mediamarktsaturn.com>
2025-05-19 22:43:56 +0200

    feat(nvim): add oil.nvim for filetree operation
    See also
    - https://github.com/stevearc/oil.nvim
    - https://www.youtube.com/watch?v=-r1mMg-yVZE

---
+++ .config/nvim/lua/plugins/oil.lua
@@ -0,0 +1,11 @@
+return {
+  'stevearc/oil.nvim', -- https://github.com/stevearc/oil.nvim
+  dependencies = { 'nvim-tree/nvim-web-devicons' },
+  lazy = false,
+  opts = {
+    view_options = {
+      show_hidden = true,
+      case_insensitive = true,
+    },
+  },
+}
