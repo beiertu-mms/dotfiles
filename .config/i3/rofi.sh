06836764ab2d8baac2b231e38bdccb5b4a544204
Tung Beier <beiertu@mediamarktsaturn.com>
2025-10-07 09:55:01 +0200

    chore: remove configurations of unused apps

--- .config/i3/rofi.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-
-rofi -modi combi -show combi -combi-modi window,run,ssh
--- .config/zsh/aliases/confluent.zsh
+++
@@ -1,3 +0,0 @@
-alias cf='confluent'
-alias cfenv='confluent environment'
-alias cfk='confluent kafka'
